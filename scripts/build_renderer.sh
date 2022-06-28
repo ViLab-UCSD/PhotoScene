9a627f5853bfcd7baa808c0d7c4e71faaea1eef3
yuyingyeh <yuying1423@gmail.com>
2022-06-27 18:35:40 -0700

    update scripts

---
+++ scripts/build_renderer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+. scripts/photoscene_utils.sh
+yamlFile="${1:-"configs/base.yaml"}"
+load_base_paths $yamlFile
+
+optix_version="5.1.1"
+
+cd $rendererDir/OptixRenderer_MatPart/src/
+rm CMakeCache.txt
+cmake -D OptiX_INSTALL_DIR=$OptixRoot/NVIDIA-OptiX-SDK-$optix_version-linux64 ../src/
+make
+
+cd $rendererDir/OptixRenderer/src/
+rm CMakeCache.txt
+cmake -D OptiX_INSTALL_DIR=$OptixRoot/NVIDIA-OptiX-SDK-$optix_version-linux64 ../src/
+make
+
+cd $rendererDir/OptixRenderer_UVcoord/src/
+rm CMakeCache.txt
+cmake -D OptiX_INSTALL_DIR=$OptixRoot/NVIDIA-OptiX-SDK-$optix_version-linux64 ../src/
+make
+
+cd $rendererDir/OptixRenderer_Light/src/
+rm CMakeCache.txt
+cmake -D OptiX_INSTALL_DIR=$OptixRoot/NVIDIA-OptiX-SDK-$optix_version-linux64 ../src/
+make
