6c47f7db0a0acb1512fee40f83111cf53db10f16
yuyingyeh <yuying1423@gmail.com>
2023-04-16 21:26:31 -0700

    Update download_total3d_data.sh

--- scripts/download_total3d_data.sh
+++ scripts/download_total3d_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Download preprocessed SUN RGBD data from Total3D (12G)
-wget https://livebournemouthac-my.sharepoint.com/:u:/g/personal/ynie_bournemouth_ac_uk/EcA66Nb1aI1KitzX7avbE10BiHGzovf3rqQebeJHmFB4QA?download=1  \
-    -P third_party/Total3D/data/sunrgbd
+wget https://tumde-my.sharepoint.com/:u:/g/personal/yinyu_nie_tum_de/Ef_cwoqSdA1DlpjcEk8PAfIBiutQTW8w6yRTlBJoGRxy5w?download=1 \
+    -O third_party/Total3D/data/sunrgbd/sunrgbd_train_test_data.tar.gz
+tar -xf third_party/Total3D/data/sunrgbd/sunrgbd_train_test_data.tar.gz -C third_party/Total3D/data/sunrgbd
